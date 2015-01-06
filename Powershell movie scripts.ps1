3febcb7fdbfcb638cd591e78ec6fe665c53141cc
Roy Stewart <roystewart@hotmail.com>
2015-01-06 22:33:22 +0800

    Added powershell files

---
+++ Powershell movie scripts.ps1
@@ -0,0 +1,10 @@
+﻿Get-Process | Export-csv procs.csv
+
+Get-Process | Export-Clixml procs.xml
+
+Get-Command -Verb show
+
+Show-SimpleAccess c:\temp
+
+Compare-Object -ReferenceObject C:\Temp -DifferenceObject C:\temp\Thumbs.db
+
